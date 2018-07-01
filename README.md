b5e2c4d990fca1cf14a1dba916a4c6bcfc170508
Subham Agarwal <me@subham.me>
2018-07-01 08:06:25 +0530

    show notification for python subreddit posts

---
+++ README.md
@@ -0,0 +1,22 @@
+### INSTALLATION
+
+#### python2
+`pip install -r requirements.txt` 
+
+#### python3
+`pip3 install -r requirements.txt` 
+
+---
+
+### RUN THE APP
+
+#### python2
+`python app.py` 
+
+#### python3
+`python3 app.py` 
+
+---
+
+### NOTE:
+install dependencies globally (not in virtualenv)
---
+++ app.py
@@ -0,0 +1,27 @@
+
+
+
+import feedparser
+import time
+
+
+rss = 'https://www.reddit.com/r/Python/.rss'
+
+
+def notify(title, message):
+    import os
+    cmd = 'ntfy -t "{0}" send "{1}"'.format(title, message)
+    os.system(cmd)
+
+
+def get_feeds():
+    feed = feedparser.parse(rss)
+    for key in feed["entries"]: 
+        notify(key['title'], key['links'][0]['href'])
+        time.sleep(20)
+
+
+if __name__ == '__main__':
+    while True:
+        get_feeds()
+        time.sleep(90)
